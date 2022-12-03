549b3a0cf93699975a13c13f1adf87f2ab1b1392
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-03 11:48:54 +0300

    add the new project workclass005

---
+++ workclass005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("input a number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+ if (N % 7 == 0 && N % 23 == 0)
+   {
+    Console.WriteLine("Yes, the number is multiple of 7 and 23 ");
+   }
+ else
+ {
+    Console.WriteLine("No, the number is not multiple of 7 and 23 ");
+ }
